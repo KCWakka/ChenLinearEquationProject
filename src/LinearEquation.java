5360cbba90314d4758755e766fb7c9e8dc1a6517
BT_2N2_19 <sample@gmail.com>
2023-10-17 14:07:49 -0400

    Code done, need to debugged

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,37 +5,82 @@ public class LinearEquation {
     private double coord1YValue;
     private double coord2XValue;
     private double coord2YValue;
-    private double slope;
-    private double roundedToHundredth (double toRound) {
-        return Math.round(toRound * 100.0) / 100.0;
-    }
+
     public LinearEquation (String coord1, String coord2) {
         this.coord1 = coord1;
         this.coord2 = coord2;
-        coord1XValue = Integer.parseInt(coord1.substring(0,coord1.indexOf(",")));
-        coord1YValue = Integer.parseInt(coord1.substring(coord1.indexOf(",")));
-        coord2XValue = Integer.parseInt(coord2.substring(0,coord2.indexOf(",")));
-        coord2YValue = Integer.parseInt(coord2.substring(coord2.indexOf(",")));
+        coord1XValue = Integer.parseInt(coord1.substring(1,coord1.indexOf(",")));
+        coord1YValue = Integer.parseInt(coord1.substring(coord1.indexOf(",")+2, coord1.length()-1));
+        coord2XValue = Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
+        coord2YValue = Integer.parseInt(coord2.substring(coord2.indexOf(",")+2, coord2.length()-1));
     }
     public double distance() {
-        return roundedToHundredth(Math.abs(Math.pow(coord2XValue - coord1XValue, 2) + Math.pow(coord2YValue - coord1YValue, 2)));
+        return roundedToHundredth(Math.sqrt(Math.pow(coord2XValue - coord1XValue, 2) + Math.pow(coord2YValue - coord1YValue, 2)));
     }
     public double slope() {
-        slope = roundedToHundredth( (coord2YValue - coord1YValue) / (coord2XValue - coord1XValue) );
+        double slope;
+        if ((coord2YValue - coord1YValue) % (coord2XValue - coord1XValue) == 0) {
+            slope = (coord2YValue - coord1YValue) / (coord2XValue - coord1XValue);
+        } else {
+            slope = roundedToHundredth( (coord2YValue - coord1YValue) / (coord2XValue - coord1XValue) );
+        }
         return slope;
     }
     public double yIntercept() {
-        return roundedToHundredth(slope() * -1 * coord1XValue + coord2YValue);
+        return coord1YValue - coord1XValue * slope();
     }
     public String equation() {
-        if (slope == 0) {
+        String yIntercept = null;
+        if (yIntercept() < 0) {
+            yIntercept = "- " + Math.abs(yIntercept());
+        } else {
+            yIntercept = "+ " + Math.abs(yIntercept());
+        }
+        if (slope() == 0) {
             return "y = " + yIntercept();
         } else {
-            return "y = " + (coord2YValue - coord1YValue) + "/" + (coord2XValue - coord1XValue) + "x + " + yIntercept();
+            if (yIntercept() == 0) {
+                if ((slope() == 1) || (slope() == -1)) {
+                    if (slope() == 1) {
+                        return "y = " + "x";
+                    } else {
+                        return "y = " + "-x";
+                    }
+                } else {
+                    if ((coord2YValue - coord1YValue) % (coord2XValue - coord1XValue) == 0) {
+                        return "y = " + (int) slope() + "x";
+                    } else {
+                        if (slope() > 0) {
+                            return "y= " + Math.abs(coord2YValue - coord1YValue) + "/" + Math.abs(coord2XValue - coord1XValue) + "x";
+                        } else {
+                            return "y= -" + (coord2YValue - coord1YValue) + "/" + Math.abs(coord2XValue - coord1XValue) + "x";
+                        }
+                    }
+                }
+            } else {
+                if ((slope() == 1) || (slope() == -1)) {
+                    if (slope() == 1) {
+                        return "y = " + "x " + yIntercept;
+                    } else {
+                        return "y = " + "-x " + yIntercept;
+                    }
+                } else {
+                    if ((coord2YValue - coord1YValue) % (coord2XValue - coord1XValue) == 0) {
+                        return "y = " + (int) slope() + "x " + yIntercept;
+                    } else {
+                        if (slope() > 0) {
+                            return "y= " + Math.abs(coord2YValue - coord1YValue) + "/" + Math.abs(coord2XValue - coord1XValue) + "x " + yIntercept;
+                        } else {
+                            return "y= -" + (coord2YValue - coord1YValue) + "/" + Math.abs(coord2XValue - coord1XValue) + "x " + yIntercept;
+                        }
+                    }
+                }
+            }
         }
     }
+
     public String coordinateForX (double x) {
-        return "(" + x + ", " + slope() * x + yIntercept() + ")";
+        return "(" + x + ", " + (slope() * x + yIntercept()) + ")";
     }
     public void lineInfo() {
         System.out.println("The two points are: " + coord1 + " and " + coord2);
@@ -43,5 +88,8 @@ public class LinearEquation {
         System.out.println("The slope of this line is: " + slope());
         System.out.println("The y-intercept of this line is: " + yIntercept());
         System.out.println("The distance between these points is " + distance());
+    }
+    private double roundedToHundredth (double toRound) {
+        return Math.round(toRound * 100.0) / 100.0;
     }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -4,6 +4,7 @@ public class LinearEquationLogic {
     private Scanner myScanner;
     private String coord1;
     private String coord2;
+    private String answer= null;
 
     public LinearEquationLogic() {
         myScanner = new Scanner(System.in);
@@ -13,26 +14,51 @@ public class LinearEquationLogic {
     }
     public void start () {
         startingCode();
+        intermission();
     }
-    public void startingCode () {
+    private void startingCode () {
         System.out.println("Welcome to the linear equation calculator!");
+        mainCode();
+    }
+    private void findYWhenX() {
+        System.out.print("Enter a value for x: ");
+        System.out.println("The point on the line is " + point.coordinateForX(myScanner.nextDouble()));
+        myScanner.nextLine();
+    }
+    private void intermission () {
+        answer= null;
+        System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+        answer = myScanner.nextLine();
+        while (answer.equals("y")) {
+            mainCode();
+            System.out.print("Would you like to enter another pair of coordinates? y/n: ");
+            answer = myScanner.nextLine();
+        }
+        System.out.println("Thank you for using the slope calculator, goodbye!");
+    }
+    private void mainCode() {
         System.out.print("Enter coordinate 1: ");
         coord1 = myScanner.nextLine();
         System.out.print("Enter coordinate 2: ");
         coord2 = myScanner.nextLine();
         point = new LinearEquation(coord1, coord2);
-        double coord1XValue = Integer.parseInt(coord1.substring(0,coord1.indexOf(",")));
-        double coord1YValue = Integer.parseInt(coord1.substring(coord1.indexOf(",")));
-        double coord2XValue = Integer.parseInt(coord2.substring(0,coord2.indexOf(",")));
-        double coord2YValue = Integer.parseInt(coord2.substring(coord2.indexOf(",")));
+        double coord1XValue = Integer.parseInt(coord1.substring(1,coord1.indexOf(",")));
+        double coord1YValue = Integer.parseInt(coord1.substring(coord1.indexOf(",")+2, coord1.length()-1));
+        double coord2XValue = Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
+        double coord2YValue = Integer.parseInt(coord2.substring(coord2.indexOf(",")+2, coord2.length()-1));
         while (coord1XValue == coord2XValue) {
-            System.out.println("These two coordinate create a null slope, please enter a new set of coordinate.");
-            System.out.println("Welcome to the linear equation calculator!");
+            System.out.println("These two coordinate create a divide by zero error, please enter a new set of coordinate.");
             System.out.print("Enter coordinate 1: ");
             coord1 = myScanner.nextLine();
             System.out.print("Enter coordinate 2: ");
             coord2 = myScanner.nextLine();
             point = new LinearEquation(coord1, coord2);
+            coord1XValue = Integer.parseInt(coord1.substring(1,coord1.indexOf(",")));
+            coord1YValue = Integer.parseInt(coord1.substring(coord1.indexOf(",")+2, coord1.length()-1));
+            coord2XValue = Integer.parseInt(coord2.substring(1,coord2.indexOf(",")));
+            coord2YValue = Integer.parseInt(coord2.substring(coord2.indexOf(",")+2, coord2.length()-1));
         }
+        point.lineInfo();
+        findYWhenX();
     }
 }
